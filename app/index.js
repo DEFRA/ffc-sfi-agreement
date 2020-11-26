8e82ba106883fb97f9661d286497f83a9bac6053
Paul Andrews <paul.andrews@defra.gov.uk>
2020-11-26 17:18:36 +0000

    Fix typo and test messaging

--- app/index.js
+++ app/index.js
@@ -3,6 +3,13 @@ const server = require('./server')
 const init = async () => {
   await server.start()
   console.log('Server running on %s', server.info.uri)
+
+  const messageAction = message => console.log(message.body)
+
+  require('./messaging/senders').agreementChanged({ test: 'Agreement changed test 123' })
+  require('./messaging/senders').eligibilityChanged({ test: 'Eligibility changed test 123' })
+  require('./messaging/receivers').startUpdateAgreement(messageAction)
+  require('./messaging/receivers').startUpdateEligibility(messageAction)
 }
 
 process.on('unhandledRejection', (err) => {
