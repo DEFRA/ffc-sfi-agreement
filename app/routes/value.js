587038fb22fe388886e95afa811bb7538bd02e24
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2021-01-18 11:36:43 +0000

    feat(logger): replace console.log with json logging

--- app/routes/value.js
+++ app/routes/value.js
@@ -1,12 +1,14 @@
+const { log } = require('../services/logger')
+
 module.exports = (server, cache) => {
   server.route({
     method: 'GET',
     path: '/value',
     handler: async (request, h) => {
       const { correlationId } = request.query
-      console.log(`Attempting to retrieve '${correlationId}' from cache.`)
+      log(`Attempting to retrieve '${correlationId}' from cache.`)
       const body = await cache.get(correlationId)
-      console.log(`Found '${JSON.stringify(body)}' for '${correlationId}'.`)
+      log(`Found '${JSON.stringify(body)}' for '${correlationId}'.`)
       return h.response({ correlationId, body }).code(200)
     }
   })
